5e1fbc911ad08c25af7680a5774fd3624b94f5f6
gabrielli <gabieven14@gmail.com>
2020-07-24 01:24:40 -0300

    Alteração responsividade tela Jogo finalizado

--- src/Components/Atoms/Titulo/Titulo.js
+++ src/Components/Atoms/Titulo/Titulo.js
@@ -5,7 +5,7 @@ import "./Titulo.css";
 
 const Titulo = (props) =>{
     return(
-        <h2 className={props.tituloClass}>{props.titulo}</h2>
+        <h2 className={props.tituloClass}>{props.titulo}<span></span></h2>
     )
 }
 
